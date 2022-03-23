750caf7b38107c172cb2ec4162c88cdcdcb0e837
tj <tjciccone28@gmail.com>
2022-03-22 20:11:48 -0400

    added selectors for digits, operators, and equals

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 // query selector for display window
 let display = document.querySelector('.display');
+
+// query selector and click event for all digits 0-9
+let digits = Array.from(document.querySelectorAll('.digit'));
+digits.map(digit => {
+    digit.addEventListener('click', () => console.log('DIGITS'));
+})
+
+// query selector and event for all operators " / + - * "
+let operators = Array.from(document.querySelectorAll('.operator'));
+operators.map(operator => {
+    operator.addEventListener('click', () => console.log('OPERATORS'));
+})
+
+// query selector and event for equals button
+let equalsButton = document.querySelector('.equals');
+equalsButton.addEventListener('click', () => console.log("EQUALS!!!"))
+
+
+let num1 = '';
+let num2 = '';
+let operator = '';
